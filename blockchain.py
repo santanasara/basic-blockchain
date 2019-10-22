2498824b3c2ad0b1621e6b42542f651521962749
santanasara <saraa.beatrizz@gmail.com>
2019-10-22 11:12:47 -0300

    blockchain.py atualizado

--- blockchain.py
+++ blockchain.py
@@ -3,174 +3,152 @@ import json
 from time import time
 import copy
 import random
+import requests
+
 from bitcoin.wallet import CBitcoinSecret
 from bitcoin.signmessage import BitcoinMessage, VerifyMessage, SignMessage
 
+DIFFICULTY = 4
+
 class Blockchain(object):
-    difficulty = 4
+
     def __init__(self):
         self.chain = []
         self.memPool = []
+        self.nodes = []
         self.createGenesisBlock()
-        
-        
 
     def createGenesisBlock(self):
-        self.createBlock()
-        genesisBlock = self.chain[-1]
-        genesisBlock['nonce'] = self.mineProofOfWork(self.prevBlock)
-    
-   
-    def createBlock(self):
-       
-        index = len(self.chain)
-        # Creates genesis block
-        if(index==0):
-            block = {
-            'index': index,
+        self.createBlock(previousHash='0'*64, nonce=0)
+        self.mineProofOfWork(self.prevBlock) 
+
+    def createBlock(self, nonce=0, previousHash=None):
+        if (previousHash == None):
+            previousBlock = self.chain[-1]
+            previousBlockCopy = copy.copy(previousBlock)
+            previousBlockCopy.pop("transactions", None)
+
+        block = {
+            'index': len(self.chain) + 1,
             'timestamp': int(time()),
-            'nonce': 0,
-            'merkleRoot': 0,
-            'previousHash': 0,
-            'transactions': self.memPool
-            }
-        # Creates other blocks
-        else:
-            
-            block = {
-                'index': index,
-                'timestamp': timestamp,
-                'nonce': 0,
-                'merkleRoot': self.generateMerkleRoot(self.memPool),
-                'previousHash': self.getBlockId(self.prevBlock),
-                'transactions': self.memPool
-            }
-            
-        
+            'transactions': self.memPool,
+            'merkleRoot': self.generateMerkleRoot(self.memPool),
+            'nonce': nonce,
+            'previousHash': previousHash or self.generateHash(previousBlockCopy),
+        }
+
+        self.memPool = []
         self.chain.append(block)
-       
+        return block
+
+    def mineProofOfWork(self, prevBlock):
+        nonce = 0
+        while self.isValidProof(prevBlock, nonce) is False:
+            nonce += 1
+
+        return nonce
+
+    def createTransaction(self, sender, recipient, amount, timestamp, privKey):
+        tx = {
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount,
+            'timestamp': timestamp
+        }
+
+        tx['signature'] = Blockchain.sign(privKey, json.dumps(tx, sort_keys=True)).decode('utf-8')
+        self.memPool.append(tx)
+
+        return self.prevBlock['index'] + 1
+
+    def isValidChain(self, chain):
+    
+        for block in chain:
+            hashBlock = self.getBlockID(block)
+
+            if (hashBlock[:DIFFICULTY] == "0" * DIFFICULTY):
+                if (block["merkleRoot"] == self.generateMerkleRoot(block["transactions"])):
+                    return True
+            else:
+                return False
+
+
+    def resolveConflicts(self):
+        
+        for node in self.nodes:
+            chain = requests.get(self.node + "/chain")
+            nodeChain = chain.json()
+            
+            if self.isValidChain(nodeChain):
+                if len(nodeChain) > len(self.chain):
+                    self.chain = nodeChain
+
+        return self.chain
+
+
+    @staticmethod
+    def generateMerkleRoot(transactions):
+        transactionsData = json.dumps(transactions, sort_keys=True)
+
+        return Blockchain.generateHash(transactionsData)
+        merkleTree = transactions.copy()
+        if(len(merkleTree) % 2 != 0):
+            merkleTree.append(merkleTree[-1])
+
+        while (len(merkleTree)> 1):
+            j = 0
+            for i in range(0, len(merkleTree) - 1):
+                merkleTree[j] = Blockchain.generateHash(str(merkleTree[i]) + str(merkleTree[i+1]))
+                i += 2
+                j += 1
+            lastDelete = i - j
+            del merkleTree[-lastDelete:]
+
+        return merkleTree
+
+
+    @staticmethod
+    def isValidProof(block, nonce):
+        block['nonce'] = nonce
+        guessHash = Blockchain.getBlockID(block)
+        return guessHash[:DIFFICULTY] == '0' * DIFFICULTY 
+
+    @staticmethod
+    def generateHash(data):
+        blkSerial = json.dumps(data, sort_keys=True).encode()
+        return hashlib.sha256(blkSerial).hexdigest()
+
+    @staticmethod
+    def getBlockID(block):
+        blockCopy = copy.copy(block)
+        blockCopy.pop("transactions", None)
+        return Blockchain.generateHash(blockCopy)
+
+    def printChain(self):
+         for i in self.chain:
+
+            block = (json.dumps(i, sort_keys=True, indent=2))
+
+            block = block.replace(",", "")
+            block = block.replace("\"", "")
+            block = block.replace("{", "")
+            block = block.replace("}", "")
+            print(block, "\n")
+        
+
     @property
     def prevBlock(self):
         return self.chain[-1]
 
     @staticmethod
-    def generateHash(data):
-        blkSerial = json.dumps(data, sort_keys=True).encode()
-        
-        return hashlib.sha256(blkSerial).hexdigest()
-    
-
-    @staticmethod
-    def getBlockId(block):
-        blockHeader = block.copy()
-        blockHeader.pop('transactions')
-        
-        return Blockchain.generateHash(blockHeader)
-        
-
-    
-    @staticmethod
-    def isValidProof(prevBlock, nonce):
-        blockCopy = prevBlock.copy()
-        blockCopy['nonce'] = nonce
-        
-        checkBlockId = Blockchain.getBlockId(blockCopy)
-
-        if(checkBlockId[:Blockchain.difficulty]=='0'*Blockchain.difficulty):
-            return True
-       
-        del blockCopy
-        return False
-        
-    def mineProofOfWork(self, prevBlock):
-        validNonce = 0
-        blockCopy = prevBlock.copy()
-        while(True):
-            validNonce+=1
-            blockCopy['nonce'] = validNonce
-            proofOfWork = self.getBlockId(blockCopy)
-            
-            if(self.isValidProof(blockCopy, validNonce)):
-                self.prevBlock['nonce'] = validNonce
-                break
-        
-        del blockCopy
-        return validNonce
-
-    def printChain(self):
-        count = 0
-        for i in self.chain:
-            
-            block = (json.dumps(i, sort_keys=True, indent=2))
-            
-            block = block.replace(",", "")
-            block = block.replace("\"", "")
-            block = block.replace("{", "")
-            block = block.replace("}", "")
-            block = block.replace("[", "")
-            block = block.replace("]", "")
-            
-            print(block, "\n")
-            count = count+1
-
-
-    @staticmethod
     def sign(privKey, message):
         secret = CBitcoinSecret(privKey)
-        message = BitcoinMessage(message)
-        return SignMessage(secret, message).decode()
-        
+        msg = BitcoinMessage(message)
+        return SignMessage(secret, msg)
         
     @staticmethod
     def verifySignature(address, signature, message):
-        
         msg = BitcoinMessage(message)
         return VerifyMessage(address, msg, signature)
 
 
-    def createTransaction(self, sender, recipient, amount, timestamp, privKey):
-        
-        message = {
-        "sender": sender,
-        "recipient": recipient,
-        "amount": amount,
-        "timestamp": timestamp,
-        }
-
-        blkSerial = json.dumps(message, sort_keys=True)
-        signature = Blockchain.sign(privKey, blkSerial)
-        
-        transaction = {
-        "sender": sender,
-        "recipient": recipient,
-        "amount": amount,
-        "timestamp": timestamp,
-        "signature": signature
-        }
-        
-        
-        self.memPool.append(transaction)
-        #print("\n", self.memPool)
-
-    @staticmethod
-    def generateMerkleRoot(transactions):
-        transactionsData = json.dumps(transactions, sort_keys=True)
-        
-        return Blockchain.generateHash(transactionsData)
-
-# Teste
-blockchain = Blockchain()
-
-sender = '19sXoSbfcQD9K66f5hwP5vLwsaRyKLPgXF'
-recipient = '1MxTkeEP2PmHSMze5tUZ1hAV3YTKu2Gh1N'
-
-# Cria 5 blocos, incluindo o Genesis, contendo de 1-4 transações cada, com valores aleatórios, entre os endereços indicados em sender e recipient.
-for x in range(0, 4): 
-    for y in range(0, random.randint(1,4)) : 
-        timestamp = int(time())
-        amount = random.uniform(0.00000001, 100)
-        blockchain.createTransaction(sender, recipient, amount, timestamp, 'L1US57sChKZeyXrev9q7tFm2dgA2ktJe2NP3xzXRv6wizom5MN1U')
-    blockchain.createBlock()
-    blockchain.mineProofOfWork(blockchain.prevBlock)
-
-blockchain.printChain()
